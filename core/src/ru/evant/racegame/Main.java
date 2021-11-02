a027d275760468ecedb194c731c39b6d9342f9fa
Anton Evseev <58934670+AntonEvseev74@users.noreply.github.com>
2021-11-02 22:55:38 +0500

    Созданы классы: - view  + GameScreen - model  + GameObject  + Car

--- core/src/ru/evant/racegame/Main.java
+++ core/src/ru/evant/racegame/Main.java
@@ -1,11 +1,17 @@
 package ru.evant.racegame;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+
+import ru.evant.racegame.view.GameScreen;
 
 public class Main extends Game {
 
-	@Override
-	public void create() {
+    private Screen gameScreen;
 
-	}
+    @Override
+    public void create() {
+        gameScreen = new GameScreen();
+		setScreen(gameScreen);
+    }
 }
--- core/src/ru/evant/racegame/view/GameScreen.java
+++ core/src/ru/evant/racegame/view/GameScreen.java
@@ -1,17 +1,37 @@
 package ru.evant.racegame.view;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import ru.evant.racegame.model.Car;
 
 public class GameScreen implements Screen {
 
+    private Texture textureCar;
+    private SpriteBatch batch;
+    private Car car;
+
     @Override
     public void show() {
-
+        batch = new SpriteBatch();
+        textureCar = new Texture(Gdx.files.internal("car.png"));
+        car = new Car(textureCar,0,0,80,150);
     }
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+
+        //!
+        batch.begin();
+            car.draw(batch);
+        batch.end();
+        //!
     }
 
     @Override
@@ -36,6 +56,7 @@ public class GameScreen implements Screen {
 
     @Override
     public void dispose() {
-
+        textureCar.dispose();
+        batch.dispose();
     }
 }
