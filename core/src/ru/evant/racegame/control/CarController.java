5de1dc9f467576595871a642a37445209ab8b5f6
Anton Evseev <58934670+AntonEvseev74@users.noreply.github.com>
2021-11-03 11:18:29 +0500

    CarController - реализована обработка движения машинки вперед назад, посредством изменения скорости. Скорость > 0 = вперед, Скорость < 0 = назад.

--- core/src/ru/evant/racegame/control/CarController.java
+++ core/src/ru/evant/racegame/control/CarController.java
@@ -1,6 +1,13 @@
 package ru.evant.racegame.control;
 
+// Класс для управления машинкой
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Polygon;
+
+import ru.evant.racegame.view.GameScreen;
 
 public class CarController {
     private Polygon carBounds;
@@ -9,7 +16,39 @@ public class CarController {
         this.carBounds = carBounds;
     }
 
+    float speedCar; // скорость машинки
+    float velocitySpeedCar = 0.02f; // скорость скорости машинки
+    float speedCarMax = 3f; // максимальная скорость машинки
     public void handle(){
+        // управление скоростью
+        if (Gdx.input.isKeyPressed(Input.Keys.UP) || Gdx.input.isKeyPressed(Input.Keys.W))
+            speedCar += velocitySpeedCar;
+        else if (Gdx.input.isKeyPressed(Input.Keys.DOWN) || Gdx.input.isKeyPressed(Input.Keys.S))
+            speedCar -= velocitySpeedCar;
+        else downSpeed();
 
+        speedCar = sliceSpeed();
+        ///
+
+        //
+
+        ///
+
+        carBounds.setPosition(carBounds.getX() + MathUtils.cosDeg(carBounds.getRotation() + 90) * speedCar * GameScreen.dt,
+                carBounds.getY() + MathUtils.sinDeg(carBounds.getRotation() + 90) * speedCar * GameScreen.dt);
+    }
+
+    // уменьшение скорости
+    private void downSpeed() {
+        if (speedCar > velocitySpeedCar) speedCar -= velocitySpeedCar;
+        else if (speedCar < velocitySpeedCar) speedCar += velocitySpeedCar;
+        else speedCar = 0f;
+    }
+
+    // ограничение скорости
+    private float sliceSpeed(){
+        if (speedCar > speedCarMax) return speedCarMax;
+        if (speedCarMax < -speedCarMax) return -speedCarMax;
+        return speedCar;
     }
 }
--- core/src/ru/evant/racegame/view/GameScreen.java
+++ core/src/ru/evant/racegame/view/GameScreen.java
@@ -10,7 +10,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import ru.evant.racegame.model.Car;
 
 public class GameScreen implements Screen {
-
+    public static float dt; // delta time
 
     private SpriteBatch batch;
 
@@ -32,6 +32,8 @@ public class GameScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        dt = delta;
 
         batch.setProjectionMatrix(camera.combined);
         //!
