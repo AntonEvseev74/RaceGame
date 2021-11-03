756db96cc70f2320374b87735003ca371eb221c1
Anton Evseev <58934670+AntonEvseev74@users.noreply.github.com>
2021-11-03 11:56:23 +0500

    CarController - реализована обработка поворота машинки влево вправо, посредством изменения угла поворота. Угол > 0 = влево, Угол < 0 = вправо. Управление движением вынесено в отдельные методы

--- core/src/ru/evant/racegame/control/CarController.java
+++ core/src/ru/evant/racegame/control/CarController.java
@@ -10,46 +10,67 @@ import com.badlogic.gdx.math.Polygon;
 import ru.evant.racegame.view.GameScreen;
 
 public class CarController {
-    private Polygon carBounds;
+    private float speedCar; // скорость машинки
+    private final float velocitySpeedCar = 10f; // скорость скорости машинки
+    private final float speedCarMax = 10f; // максимальная скорость машинки
+    private final float rotationSpeedCar = 30f; // скорость поворота машинки, за 1 сек поворот будет происходить на 30 градусов
+
+    private final Polygon carBounds;
 
     public CarController(Polygon carBounds) {
         this.carBounds = carBounds;
     }
 
-    float speedCar; // скорость машинки
-    float velocitySpeedCar = 0.02f; // скорость скорости машинки
-    float speedCarMax = 3f; // максимальная скорость машинки
-    public void handle(){
-        // управление скоростью
-        // Скорость > 0 = вперед, Скорость < 0 = назад.
+    public void handle() {
+        speedCar();// управление скоростью
+        turnCar();// управление поворотом машинки
+        setCarPosition();// установить позицию машинки
+    }
+
+    /* Установить позицию машинки на экране */
+    private void setCarPosition() {
+        carBounds.setPosition
+                (
+                        carBounds.getX() + MathUtils.cosDeg(carBounds.getRotation() + 90) * speedCar * GameScreen.dt,
+                        carBounds.getY() + MathUtils.sinDeg(carBounds.getRotation() + 90) * speedCar * GameScreen.dt
+                );
+    }
+
+    /* управление поворотом машинки
+     Угол > 0 = влево, Угол < 0 = вправо.
+     */
+    private void turnCar() {
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT) || Gdx.input.isKeyPressed(Input.Keys.A))
+            carBounds.rotate(rotationSpeedCar * speedCar * GameScreen.dt); // Угол > 0 = влево
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) || Gdx.input.isKeyPressed(Input.Keys.D))
+            carBounds.rotate(-rotationSpeedCar * speedCar * GameScreen.dt); // Угол < 0 = вправо
+    }
+
+    /* управление скоростью
+     Скорость > 0 = вперед, Скорость < 0 = назад.
+     */
+    private void speedCar() {
         if (Gdx.input.isKeyPressed(Input.Keys.UP) || Gdx.input.isKeyPressed(Input.Keys.W))
-            speedCar += velocitySpeedCar; // Скорость > 0 = вперед
+            speedCar += velocitySpeedCar * GameScreen.dt; // Скорость > 0 = вперед
         else if (Gdx.input.isKeyPressed(Input.Keys.DOWN) || Gdx.input.isKeyPressed(Input.Keys.S))
-            speedCar -= velocitySpeedCar; // Скорость < 0 = назад
+            speedCar -= velocitySpeedCar * GameScreen.dt; // Скорость < 0 = назад
         else downSpeed();
 
         speedCar = sliceSpeed();
-        ///
-
-        //
-
-        ///
-
-        carBounds.setPosition(carBounds.getX() + MathUtils.cosDeg(carBounds.getRotation() + 90) * speedCar * GameScreen.dt,
-                carBounds.getY() + MathUtils.sinDeg(carBounds.getRotation() + 90) * speedCar * GameScreen.dt);
     }
 
-    // уменьшение скорости
+    /* уменьшение скорости */
     private void downSpeed() {
-        if (speedCar > velocitySpeedCar) speedCar -= velocitySpeedCar;
-        else if (speedCar < velocitySpeedCar) speedCar += velocitySpeedCar;
+        if (speedCar > velocitySpeedCar * GameScreen.dt)
+            speedCar -= velocitySpeedCar * GameScreen.dt;
+        else if (speedCar < velocitySpeedCar * GameScreen.dt)
+            speedCar += velocitySpeedCar * GameScreen.dt;
         else speedCar = 0f;
     }
 
-    // ограничение скорости
-    private float sliceSpeed(){
+    /* ограничение скорости */
+    private float sliceSpeed() {
         if (speedCar > speedCarMax) return speedCarMax;
-        if (speedCarMax < -speedCarMax) return -speedCarMax;
-        return speedCar;
+        return Math.max(speedCar, -speedCarMax);
     }
 }
