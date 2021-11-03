39d9e98a8300462f3462d7c519c8e719fe60d6c1
Anton Evseev <58934670+AntonEvseev74@users.noreply.github.com>
2021-11-03 10:57:31 +0500

    Создан класс CarController и добавлен в класс Car

--- core/src/ru/evant/racegame/control/CarController.java
+++ core/src/ru/evant/racegame/control/CarController.java
@@ -1,4 +1,15 @@
 package ru.evant.racegame.control;
 
+import com.badlogic.gdx.math.Polygon;
+
 public class CarController {
+    private Polygon carBounds;
+
+    public CarController(Polygon carBounds) {
+        this.carBounds = carBounds;
+    }
+
+    public void handle(){
+
+    }
 }
--- core/src/ru/evant/racegame/model/Car.java
+++ core/src/ru/evant/racegame/model/Car.java
@@ -2,10 +2,22 @@ package ru.evant.racegame.model;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import ru.evant.racegame.control.CarController;
 
 public class Car extends GameObject{
 
+    CarController carController;
+
     public Car(Texture texture, float x, float y, float width, float height) {
         super(texture, x, y, width, height);
+        carController = new CarController(getBounds());
+    }
+
+    @Override
+    public void draw(SpriteBatch batch) {
+        super.draw(batch);
+        carController.handle();
     }
 }
